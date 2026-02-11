94a2a61400ececf12112e5401b3eef61912b297c
jrpelegrina <juapesai@hotmail.com>
2026-02-11 10:31:25 +0100

    clean

--- search/js/main.js
+++ search/js/main.js
@@ -1,34 +1,10 @@
-function escapeHtml(unsafe) {
-    return unsafe
-         .replace(/&/g, "&amp;")
-         .replace(/</g, "&lt;")
-         .replace(/>/g, "&gt;")
-         .replace(/"/g, "&quot;")
-         .replace(/'/g, "&#039;");
- }
+async function checkServer(url,elementId){
 
-function rss(){
-    list_rss = ["https://portal.edu.gva.es/cvtic/va/category/inici/feed/","https://portal.edu.gva.es/lliurex/?feed=rss2"];
-    for( x in list_rss){
-        console.log(x);
-        fetch("https://cors-anywhere.herokuapp.com/"+list_rss[x])
-        .then(response => response.text())
-        .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-        .then(data => {
-            console.log(data);
-            const items = data.querySelectorAll("item");
-            let html = document.querySelector("#rss").innerHTML;
-            items.forEach(el => {
-                let stringscaped = escapeHtml(el.querySelector("description").childNodes[0].nodeValue);
-            html += `
-                <div class="row">
-                    <a href="${el.querySelector("link").innerHTML}" title="${stringscaped}">${el.querySelector("title").innerHTML}</a>
-    
-                </div>
-            `;
-            document.querySelector("#rss").innerHTML = html;
-            });
-        });
+    try{
+        const response=await fetch(url,{method:'HEAD',mode:'no-cors',cache:'no-store'});
+            document.getElementById(elementId).style.visibility="visible";
+    }catch(error){
+         console.log(`URL ${url} not accesible`)   
     }
 }
-rss();
+checkServer('http://server','conditional-site')
