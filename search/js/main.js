c22327ad3cf0993ec570b5a1b96a2868cbcc19b2
jrpelegrina <juapesai@hotmail.com>
2026-02-13 09:21:48 +0100

    Fixed js

--- search/js/main.js
+++ search/js/main.js
@@ -1,10 +1,10 @@
 async function checkServer(url,elementId){
 
     try{
-        const response=await fetch(url,{method:'HEAD',mode:'no-cors',cache:'no-store'});
+        const response=await fetch(url,{method:'HEAD',mode:'no-cors',cache:'no-store',targetAddressSpace:'local'});
             document.getElementById(elementId).style.visibility="visible";
     }catch(error){
          console.log(`URL ${url} not accesible`)   
     }
 }
-checkServer('http://server','conditional-site')
+checkServer('http://server/easy-sites','conditional-site')
