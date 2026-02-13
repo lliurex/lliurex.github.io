8ed9340476fe3a9ab47fb84e508b828469176567
jrpelegrina <juapesai@hotmail.com>
2026-02-13 10:51:21 +0100

    Fixed js

--- search/js/main.js
+++ search/js/main.js
@@ -1,7 +1,7 @@
 async function checkServer(url,elementId){
 
     try{
-        const response=await fetch(url,{method:'HEAD',mode:'no-cors',cache:'no-store',targetAddressSpace:'private'});
+        const response=await fetch(url,{method:'HEAD',mode:'no-cors',cache:'no-store',targetAddressSpace:'local'});
             document.getElementById(elementId).style.visibility="visible";
     }catch(error){
        console.log(`URL ${url} not accesible`)   
