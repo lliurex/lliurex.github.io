44e53ae76755855986bae37e939fe8942b624830
jrpelegrina <juapesai@hotmail.com>
2026-02-13 10:42:19 +0100

    Fixed js

--- search/js/main.js
+++ search/js/main.js
@@ -1,20 +1,10 @@
 async function checkServer(url,elementId){
 
-    console.log("INICIANDO CHECk");
     try{
-        console.log("CHEKING");
-        const response=await fetch(url,{method:'HEAD',mode:'no-cors',cache:'no-store',targetAddressSpace:'local'});
+        const response=await fetch(url,{method:'HEAD',mode:'no-cors',cache:'no-store',targetAddressSpace:'private'});
             document.getElementById(elementId).style.visibility="visible";
     }catch(error){
-        console.log("ERROR");
-        console.log(error.message);
-        if (error.message.includes('CORS policy')){
-            console.log("CONTIENE CORS");
-            document.getElementById(elementId).style.visibility="visible";
-        }else{
-            console.log("OTRO ERROR");
-            console.log(`URL ${url} not accesible`)   
-        }
+       console.log(`URL ${url} not accesible`)   
     }
 }
 checkServer('http://server','conditional-site')
