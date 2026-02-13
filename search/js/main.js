4a673dfbc47191760788d6861499dafe86fc78c1
jrpelegrina <juapesai@hotmail.com>
2026-02-13 09:45:18 +0100

    Fixed js

--- search/js/main.js
+++ search/js/main.js
@@ -4,7 +4,11 @@ async function checkServer(url,elementId){
         const response=await fetch(url,{method:'HEAD',mode:'no-cors',cache:'no-store',targetAddressSpace:'local'});
             document.getElementById(elementId).style.visibility="visible";
     }catch(error){
-         console.log(`URL ${url} not accesible`)   
+        if (error.message.contains('CORS policy')){
+            document.getElementById(elementId).style.visibility="visible";
+        }else{
+            console.log(`URL ${url} not accesible`)   
+        }
     }
 }
 checkServer('http://server','conditional-site')
